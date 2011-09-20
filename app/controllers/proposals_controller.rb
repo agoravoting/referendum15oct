96454946e71cbb4c6ca8cfeadb5724f371e516e0
Eduardo Robles Elvira <edulix@gmail.com>
2011-09-20 15:09:55 +0200

    adapting to last changes in applet

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -4,15 +4,6 @@ class ProposalsController < InheritedResources::Base
     @public_key = Verificatum.combined_public_key
     index!
   end
-
-  # GET /proposals/1/public_key
-  def public_key
-    @proposal = Proposal.find(params[:id])
-
-    respond_to do |format|
-      format.html { render :text => @proposal.public_key }
-    end
-  end
   
   def bulletin_board
     @proposal = Proposal.find(params[:id])
