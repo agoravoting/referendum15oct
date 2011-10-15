984271130c891f73121c4722ae3e1b84ac722651
rgarcia <voodoorai2000@gmail.com>
2011-10-15 04:49:16 +0200

    legal stuff

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,7 @@ Referendum15oct::Application.routes.draw do
 
   resources :votes
 
-  match '/legal' => 'pages#legal', :as => 'legal'
   match '/privacidad'  => 'pages#privacy', :as => 'privacy'
-  match '/sistema-de-votacion'  => 'pages#system_info', :as => 'system_info'
   match '/retrieve_dni'  => 'pages#retrieve_dni', :as => 'retrieve_dni'
   match '/confirmation'  => 'pages#confirmation', :as => 'confirmation'
   match '/process_ballots' => 'proposals#process_ballots'
